031b56df90bb6947c77a17aa51a7d89b5033ec63
Jeff <35370144+Hardy0529@users.noreply.github.com>
2020-07-14 15:31:05 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var s = skrollr.init();
+
+
+$(window).scroll(function(evt){
+  if($(window).scrollTop()>0){
+    $(".navbar").addClass("nav-change");
+  }else{
+    $(".navbar").removeClass("nav-change");
+  }
+});
+
+
+
+
+//緩慢滑動
+$(document).on('click', 'a', function (event) {
+  event.preventDefault();
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
